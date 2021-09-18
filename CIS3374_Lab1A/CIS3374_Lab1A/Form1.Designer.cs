d3bdd80732d0cbfbccde26da773efa1b4f797b46
frankghato <59925619+frankghato@users.noreply.github.com>
2021-09-17 22:52:55 -0400

    Add files via upload

---
+++ CIS3374_Lab1A/CIS3374_Lab1A/Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿
+namespace CIS3374_Lab1A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtDateOfBirth = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtExpectedGraduationDate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTUID = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.radUndergraduate = new System.Windows.Forms.RadioButton();
+            this.radGraduate = new System.Windows.Forms.RadioButton();
+            this.radMale = new System.Windows.Forms.RadioButton();
+            this.radFemale = new System.Windows.Forms.RadioButton();
+            this.radOther = new System.Windows.Forms.RadioButton();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtMajor = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(242, 84);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 22);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(242, 125);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(100, 22);
+            this.txtMiddleName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(242, 165);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 22);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // txtDateOfBirth
+            // 
+            this.txtDateOfBirth.Location = new System.Drawing.Point(242, 203);
+            this.txtDateOfBirth.Name = "txtDateOfBirth";
+            this.txtDateOfBirth.Size = new System.Drawing.Size(100, 22);
+            this.txtDateOfBirth.TabIndex = 3;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(659, 81);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtPhoneNumber.TabIndex = 4;
+            // 
+            // txtExpectedGraduationDate
+            // 
+            this.txtExpectedGraduationDate.Location = new System.Drawing.Point(659, 125);
+            this.txtExpectedGraduationDate.Name = "txtExpectedGraduationDate";
+            this.txtExpectedGraduationDate.Size = new System.Drawing.Size(100, 22);
+            this.txtExpectedGraduationDate.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Middle Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(108, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Date of Birth:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(411, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(211, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Phone Number (###-###-####):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(411, 125);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(179, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Expected Graduation Date:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(411, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "TUID:";
+            // 
+            // txtTUID
+            // 
+            this.txtTUID.Location = new System.Drawing.Point(659, 165);
+            this.txtTUID.Name = "txtTUID";
+            this.txtTUID.Size = new System.Drawing.Size(100, 22);
+            this.txtTUID.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(411, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Email Address:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(108, 251);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 17);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Major:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(91, 293);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 17);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Graduate Status";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(258, 293);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 17);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Gender";
+            // 
+            // radUndergraduate
+            // 
+            this.radUndergraduate.AutoSize = true;
+            this.radUndergraduate.Location = new System.Drawing.Point(6, 51);
+            this.radUndergraduate.Name = "radUndergraduate";
+            this.radUndergraduate.Size = new System.Drawing.Size(125, 21);
+            this.radUndergraduate.TabIndex = 18;
+            this.radUndergraduate.TabStop = true;
+            this.radUndergraduate.Text = "Undergraduate";
+            this.radUndergraduate.UseVisualStyleBackColor = true;
+            // 
+            // radGraduate
+            // 
+            this.radGraduate.AutoSize = true;
+            this.radGraduate.Location = new System.Drawing.Point(6, 10);
+            this.radGraduate.Name = "radGraduate";
+            this.radGraduate.Size = new System.Drawing.Size(89, 21);
+            this.radGraduate.TabIndex = 19;
+            this.radGraduate.TabStop = true;
+            this.radGraduate.Text = "Graduate";
+            this.radGraduate.UseVisualStyleBackColor = true;
+            // 
+            // radMale
+            // 
+            this.radMale.AutoSize = true;
+            this.radMale.Location = new System.Drawing.Point(19, 10);
+            this.radMale.Name = "radMale";
+            this.radMale.Size = new System.Drawing.Size(59, 21);
+            this.radMale.TabIndex = 20;
+            this.radMale.TabStop = true;
+            this.radMale.Text = "Male";
+            this.radMale.UseVisualStyleBackColor = true;
+            // 
+            // radFemale
+            // 
+            this.radFemale.AutoSize = true;
+            this.radFemale.Location = new System.Drawing.Point(19, 37);
+            this.radFemale.Name = "radFemale";
+            this.radFemale.Size = new System.Drawing.Size(75, 21);
+            this.radFemale.TabIndex = 21;
+            this.radFemale.TabStop = true;
+            this.radFemale.Text = "Female";
+            this.radFemale.UseVisualStyleBackColor = true;
+            // 
+            // radOther
+            // 
+            this.radOther.AutoSize = true;
+            this.radOther.Location = new System.Drawing.Point(19, 64);
+            this.radOther.Name = "radOther";
+            this.radOther.Size = new System.Drawing.Size(65, 21);
+            this.radOther.TabIndex = 22;
+            this.radOther.TabStop = true;
+            this.radOther.Text = "Other";
+            this.radOther.UseVisualStyleBackColor = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(659, 203);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 23;
+            // 
+            // txtMajor
+            // 
+            this.txtMajor.Location = new System.Drawing.Point(242, 246);
+            this.txtMajor.Name = "txtMajor";
+            this.txtMajor.Size = new System.Drawing.Size(100, 22);
+            this.txtMajor.TabIndex = 24;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(414, 321);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 25;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(547, 321);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(684, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Quit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radGraduate);
+            this.groupBox1.Controls.Add(this.radUndergraduate);
+            this.groupBox1.Location = new System.Drawing.Point(84, 313);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(149, 100);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radMale);
+            this.groupBox2.Controls.Add(this.radFemale);
+            this.groupBox2.Controls.Add(this.radOther);
+            this.groupBox2.Location = new System.Drawing.Point(242, 313);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(143, 100);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtMajor);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtTUID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtExpectedGraduationDate);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtDateOfBirth);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Student Profile Program - Frank Gatto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtDateOfBirth;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtExpectedGraduationDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTUID;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RadioButton radUndergraduate;
+        private System.Windows.Forms.RadioButton radGraduate;
+        private System.Windows.Forms.RadioButton radMale;
+        private System.Windows.Forms.RadioButton radFemale;
+        private System.Windows.Forms.RadioButton radOther;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtMajor;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ CIS3374_Lab1A/CIS3374_Lab1A/Form1.cs
@@ -0,0 +1,224 @@
+﻿/*
+ * Frank Gatto
+ * 9/21/21
+ * CIS 3374 Lab 1A - Student Profile Program
+ * Allows the user to enter a student's information and then writes it to a file
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CIS3374_Lab1A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool canSubmit = false;
+            bool isTUIDValid = false;
+            bool isBirthDayValid = false;
+            bool isBirthMonthValid = false;
+            bool isBirthYearValid = false;
+            bool isPhoneNumberValid = false;
+            bool isGenderValid = false;
+
+  
+            int TUID = 0;
+            //validate tuid
+            if (txtTUID.TextLength == 9 && int.TryParse(txtTUID.Text, out TUID))
+            {
+                isTUIDValid = true;
+            }
+            else
+            {
+                MessageBox.Show("You must enter a valid 9 digit TUID number.", "TUID Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            int month, day, year = 0;
+            //validate birthday
+            if (txtDateOfBirth.TextLength == 10)
+            {
+                if (int.TryParse(txtDateOfBirth.Text.Substring(0, 2), out month) && month >= 1 && month <= 12)
+                {
+                    isBirthMonthValid = true;
+                }
+                else
+                {
+                    MessageBox.Show("You must enter a valid date of birth.", "Birth Month Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                //assuming that a student can only be at least 16 and at most 61
+                if (int.TryParse(txtDateOfBirth.Text.Substring(6, 4), out year) && year <= 2005 && year >= 1960)
+                {
+                    isBirthYearValid = true;
+                }
+                else
+                {
+                    MessageBox.Show("You must enter a valid year of birth.", "Birth Year Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                if (int.TryParse(txtDateOfBirth.Text.Substring(3, 2), out day))
+                {
+                    //if day can be parsed, check to see if it is allowed within the month
+                    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                    {
+                        if (day >= 1 && day <= 31)
+                        {
+                            isBirthDayValid = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("You must enter a valid date of birth.", "Birth Day Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else if (month == 2)
+                    {
+                        if (day >= 1 && day <= 28)
+                        {
+                            isBirthDayValid = true;
+                        }
+                        //29 days allowed if year is evenly divisble by 4 because of leap year
+                        else if (day == 29 && year % 4 == 0)
+                        {
+                            isBirthDayValid = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("You must enter a valid date of birth.", "Birth Day Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else if (month == 4 || month == 6 || month == 9 || month == 11)
+                    {
+                        if (day >= 1 && day <= 30)
+                        {
+                            isBirthDayValid = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("You must enter a valid date of birth.", "Birth Day Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("You must enter a valid date of birth.", "Birth Day Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must enter a valid date of birth.", "Invalid Date of Birth Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            int firstThreeDigits, secondThreeDigits, lastFourDigits;
+            if(txtPhoneNumber.TextLength == 12 && int.TryParse(txtPhoneNumber.Text.Substring(0,3), out firstThreeDigits) && int.TryParse(txtPhoneNumber.Text.Substring(4,3), out secondThreeDigits) && int.TryParse(txtPhoneNumber.Text.Substring(8,4), out lastFourDigits))
+            {
+                isPhoneNumberValid = true;
+            }
+            else
+            {
+                MessageBox.Show("You must enter a valid phone number.", "Phone Number Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if(radFemale.Checked || radMale.Checked || radOther.Checked)
+            {
+                isGenderValid = true;
+            }
+            else
+            {
+                MessageBox.Show("You must select a gender option.", "Gender unselected Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //if all required fields are successfully validated, call the function to write to file
+            if(isTUIDValid && isPhoneNumberValid && isBirthDayValid && isBirthMonthValid && isBirthYearValid && isGenderValid)
+            {
+                String fileName = "student_profiles.txt";
+                if (!File.Exists(fileName))
+                {
+                    File.Create(fileName).Close();
+                }
+                using (StreamWriter writer = File.AppendText(fileName))
+                {
+                    WriteStudentInfoToFIle(writer, year);
+                }
+                MessageBox.Show("Student's information written to file.", "Success!", MessageBoxButtons.OK);
+                ClearAll();
+            }
+        }
+
+        //function that writes all student's information to file
+        public void WriteStudentInfoToFIle(StreamWriter writer, int year)
+        {
+            writer.WriteLine("Name: " + txtLastName.Text + ", " + txtFirstName.Text + " " + txtMiddleName.Text);
+            writer.WriteLine("Age: " + (2021 - year).ToString());
+            writer.WriteLine("TUID: " + txtTUID.Text);
+            writer.WriteLine("Email Address: " + txtEmail.Text);
+            writer.WriteLine("Phone Number: " + txtPhoneNumber.Text);
+            writer.WriteLine("Major: " + txtMajor.Text);
+            writer.WriteLine("Expected Graduation Date: " + txtExpectedGraduationDate.Text);
+            writer.Write("Undergraduate or Graduate: ");
+            if(radGraduate.Checked)
+            {
+                writer.WriteLine("Graduate");
+            }
+            else
+            {
+                writer.WriteLine("Undergraduate");
+            }
+            writer.Write("Gender: ");
+            if(radMale.Checked)
+            {
+                writer.WriteLine("Male");
+            }
+            else if(radFemale.Checked)
+            {
+                writer.WriteLine("Female");
+            }
+            else
+            {
+                writer.WriteLine("Other");
+            }
+
+        }
+
+        //clear every potentially filled in field
+        public void ClearAll()
+        {
+            txtFirstName.Text = "";
+            txtMiddleName.Text = "";
+            txtLastName.Text = "";
+            txtDateOfBirth.Text =  "";
+            txtPhoneNumber.Text = "";
+            txtExpectedGraduationDate.Text = "";
+            txtTUID.Text = "";
+            txtEmail.Text = "";
+            txtMajor.Text = "";
+            radUndergraduate.Checked = false;
+            radGraduate.Checked = false;
+            radMale.Checked = false;
+            radFemale.Checked = false;
+            radOther.Checked = false;
+        }
+    }
+}
